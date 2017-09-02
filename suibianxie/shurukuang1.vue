5825c746a5c7424fd3dfbb7e09e9bf1964ee7792
unknown <lengmeiyumo>
2017-09-03 00:31:21 +0800

    输入框搜索

---
+++ suibianxie/shurukuang1.vue
@@ -0,0 +1,37 @@
+<template>
+    <div >
+        <input type="text" v-model="name">
+        <ul>
+            <li v-for="user in newUsers" >
+                {{ user.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default{
+        name:"Select",
+        data() {
+            return {
+              name: '',
+              users:[
+                        { name: 'Bruce' },
+                        { name: 'Chuck' },
+                        { name: 'Jackie' },
+                        { name: 'lili' }
+              ] 
+            }
+           
+        },
+        computed: {
+                newUsers:function () {
+                    var that = this;
+                    return that.users.filter((user)=>{
+                        return user.name.toLowerCase().indexOf(that.name.toLowerCase()) !== -1;
+                    })
+                }
+
+            }
+        }
+</script>  
